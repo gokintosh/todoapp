4a65de00fb67771826896c770920b4efef1c9fb9
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-04-08 22:36:22 +0200

    this is done

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+const form=document.querySelector('form');
+const ul=document.querySelector('ul');
+const button=document.querySelector('button');
+const input=document.getElementById('item');
+
+let itemsArray=localStorage.getItem('items')?JSON.parse(localStorage.getItem('items')):[];
+const data=JSON.parse(localStorage.getItem('items'));
+
+
+
+
+const liMaker=text=>{
+    const li=document.createElement('li');
+    li.textContent=text;
+    ul.prepend(li);
+}
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    itemsArray.push(input.value);
+    localStorage.setItem('items',JSON.stringify(itemsArray))
+    
+    liMaker(input.value)
+    input.value=' ';
+
+})
+
+data.forEach(item => {
+    liMaker(item);
+});
+
+// for(i=(data.length-1);i>=0;i--){
+//     liMaker(data[i])
+// }
+
+button.addEventListener('click',function(){
+    localStorage.clear();
+    while(ul.firstChild){
+        ul.removeChild(ul.firstChild)
+        
+    }
+})
